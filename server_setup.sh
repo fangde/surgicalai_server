8a13d50c44d4f6ea38508a92016ea5e16c839e1e
wjz <hibana1983@gmail.com>
2018-04-08 14:33:39 +0800

    add install script

--- server_setup.sh
+++ server_setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 wget https://download.slicer.org/bitstream/738960
-
+wget https://repo.continuum.io/archive/Anaconda2-5.1.0-Linux-x86_64.sh
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.1.85-1_amd64.deb
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sudo apt-get install cuda-9-0
+pip install tensorflow-gpu
+pip install tensorlayer
+pip install pycuda
